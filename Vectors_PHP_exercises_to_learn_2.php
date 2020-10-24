ffd76a080174e82268738460c9f5dd76586bc5a0
unknown <acuevas3@hotmail.es>
2020-10-24 09:32:55 +0200

    Mateix exercici però utilitzant el foreach

---
+++ Vectors_PHP_exercises_to_learn_2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+$dies = array(
+  'Dilluns',
+  'Dimarts',
+  'Dimecres',
+  'Dijous',
+  'Divendres',
+  'Dissabte',
+  'Diumenge');
+
+foreach ($dies as $dia) {
+  echo "$dia<br>";
+}
+
+?>
+
+</body>
+</html>
