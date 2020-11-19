1be9e76863df1671be9fa9635c92236c15680b65
Adrián <acuevas3@hotmail.es>
2020-11-19 21:00:42 +0100

    Functions_PHP_exercises_2_validation

---
+++ function/form_intermediate_validation.php
@@ -0,0 +1,26 @@
+<?php
+
+//Funcio 1
+function email_validacio($valor){
+  return filter_var($valor,FILTER_VALIDATE_EMAIL);
+}
+
+
+if (!email_validacio("adrian@cuevas.com")) {
+  echo "email erroni";
+}
+
+echo "<br><br>";
+
+//Funcio 2
+function passwordvalidation($valor1,$valor2){
+  if (strcmp($valor1,$valor2) == 0)
+    return true;
+  return false;
+}
+
+if (!passwordvalidation("Hola", "hola")) {
+  echo "no són iguals <br><br>";
+}
+
+?>
